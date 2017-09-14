8c3732acdf072c7c55ec37cb6b9629d891db1317
Eric Natelson <natelsoneric@gmail.com>
2017-09-14 08:47:40 -0400

    Update botchart.py

--- botchart.py
+++ botchart.py
@@ -13,7 +13,7 @@ class BotChart(object):
 		self.data = []
 
 		if (exchange == "poloniex"):
-			self.conn = poloniex('ID8SVZR495A6A44RPKXPWVSGR1ANXPH6','518b0335e8931b8a4d37286728ed582b8ba4eb96a099a20677167ddc9ded911ab5c8217c0c1dfe6665a833128193576ea3ae1e8ab96f9f623ce271663f9c7148')
+			self.conn = poloniex('','')
 
 			if backtest:
 				self.data = self.conn.api_query("returnChartData",{"currencyPair":self.pair,"start":self.startTime,"end":self.endTime,"period":self.period})
